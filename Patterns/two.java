17c408e0286cb92b23e015e741a2a60e93b4e2b5
Souvik Bhattacharya <souvikofficial2004@gmail.com>
2026-05-23 23:46:18 +0530

    (Patterns): add pattern 2

---
+++ Patterns/two.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class two {
+
+    // Method to print the pattern
+    public static void pattern2(int rows) {
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking number of rows from user
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        // Calling method
+        pattern2(rows);
+
+        sc.close();
+    }
+}
